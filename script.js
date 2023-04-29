2cb465b8b4b669240764f79cd03a95bfe7614181
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 22:57:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function highlight() {
     //Write your code here
-
+const boldElements = document.getElementsByTagName("strong");
+  for (let i = 0; i < boldElements.length; i++) {
+    boldElements[i].style.color = "green";
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+const boldElements = document.getElementsByTagName("strong");
+  for (let i = 0; i < boldElements.length; i++) {
+    boldElements[i].style.color = "black";
+  }
     
 }
+const linkElement = document.getElementById("link");
+const textElement = document.getElementById("text");
+
+linkElement.addEventListener("mouseover", highlight);
+linkElement.addEventListener("mouseout", return_normal);
